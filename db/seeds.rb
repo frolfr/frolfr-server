bc82a44b60f64e498d597d21feca8c96101fcdaa
Geoff Schorkopf <gschorkopf@gmail.com>
2014-02-26 20:01:52 -0500

    Adds a seed data object for Perkerson Park course

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Course.create(
+  name: "Perkerson Park",
+  city: "Atlanta",
+  state: "GA",
+  country: "United States"
+)
