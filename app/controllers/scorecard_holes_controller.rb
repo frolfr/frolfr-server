9eebb546c547a337f69ca27a1493555ccf1c6d0f
Geoff Schorkopf <gschorkopf@gmail.com>
2014-03-01 16:10:54 -0500

    Adds forms to the view layer

--- app/controllers/scorecard_holes_controller.rb
+++ app/controllers/scorecard_holes_controller.rb
@@ -2,10 +2,10 @@ class ScorecardHolesController < ApplicationController
   def update
     scorecard_hole = ScorecardHole.find(params[:id])
     scorecard_hole.update(score: params[:scorecard_hole][:score])
+    path = params[:scorecard_hole][:locale] == 'de' ? root_path(locale: 'de') : root_path
 
     respond_to do |format|
-      format.html { redirect_to root_path, notice: 'Scorecard was successfully updated.' }
-      format.json { respond_with_bip(scorecard_hole) }
+      format.html { redirect_to path, notice: 'Scorecard was successfully updated.' }
     end
   end
 end
