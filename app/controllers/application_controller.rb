47de178c046adb4af7404bdc6020c9987c8921be
Geoff Schorkopf <gschorkopf@gmail.com>
2014-03-02 18:14:59 -0500

    Adds pieces for mobile navigation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  before_action :set_locale
+  before_action :set_locale, :prepare_for_mobile
+  helper_method :mobile_device?
  
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  private
+
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param].to_s == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+
+  def prepare_for_mobile
+    session[:mobile_param] = params[:mobile] if params[:mobile]
+    request.format = :mobile if mobile_device?
   end
 end
