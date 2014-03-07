61aa9c6a134b1f2c46c7121cb050363a53133449
Geoff Schorkopf <gschorkopf@gmail.com>
2014-03-07 00:01:38 -0500

    Adds a default scope to holes

--- app/models/hole.rb
+++ app/models/hole.rb
@@ -4,4 +4,6 @@ class Hole < ActiveRecord::Base
   validates :course, :number, presence: true
   validates :number, uniqueness: true
   validates :number, inclusion: { in: (1..27) }
+
+  default_scope order('number ASC')
 end
