2a96b60ab849ff0e3df3fd7d9aa1fbf3c9b72fb7
Geoff Schorkopf <gschorkopf@gmail.com>
2014-03-07 00:01:49 -0500

    Adds back some missing translations

--- app/decorators/round_decorator.rb
+++ app/decorators/round_decorator.rb
@@ -2,6 +2,10 @@ class RoundDecorator < Draper::Decorator
   delegate_all
   decorates_association :course
 
+  def scorecard_title
+    I18n.t('round.scorecard_title', name: course.name)
+  end
+
   def location
     course.location
   end
