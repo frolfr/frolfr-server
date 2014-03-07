596df92901976881226f9b4c9d12e96ce1c81968
Geoff Schorkopf <gschorkopf@gmail.com>
2014-03-06 23:19:20 -0500

    Reworks home page for mobile; removes lots of unused crap

--- app/controllers/scorecard_holes_controller.rb
+++ app/controllers/scorecard_holes_controller.rb
@@ -1,11 +1,6 @@
 class ScorecardHolesController < ApplicationController
-  def update
-    scorecard_hole = ScorecardHole.find(params[:id])
-    scorecard_hole.update(score: params[:scorecard_hole][:score])
-    path = params[:scorecard_hole][:locale] == 'de' ? root_path(locale: 'de') : root_path
-
-    respond_to do |format|
-      format.html { redirect_to path, notice: 'Scorecard was successfully updated.' }
-    end
+  def show
+    @scorecard_hole = ScorecardHole.find(params[:id]).decorate
+    @round = @scorecard_hole.round
   end
 end
--- app/decorators/round_decorator.rb
+++ app/decorators/round_decorator.rb
@@ -2,10 +2,6 @@ class RoundDecorator < Draper::Decorator
   delegate_all
   decorates_association :course
 
-  def scorecard_title
-    I18n.t('round.scorecard_title', name: course.name)
-  end
-
   def location
     course.location
   end
