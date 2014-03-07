67dd29c2dbdfb478834d87238200786307510667
Geoff Schorkopf <gschorkopf@gmail.com>
2014-03-06 23:27:20 -0500

    Cleans up some more views, adds strings to translation file

--- app/decorators/scorecard_hole_decorator.rb
+++ app/decorators/scorecard_hole_decorator.rb
@@ -2,6 +2,6 @@ class ScorecardHoleDecorator < Draper::Decorator
   delegate_all
 
   def hole_number
-    I18n.t('hole.number', hole.number)
+    I18n.t('hole.number', number: hole.number)
   end
 end
