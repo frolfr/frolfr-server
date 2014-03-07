07a5c3bfe7abaa64defbf1bb782a6acc04495827
Geoff Schorkopf <gschorkopf@gmail.com>
2014-03-06 23:22:52 -0500

    Adds some validations for par in scorecard hole

--- app/models/scorecard_hole.rb
+++ app/models/scorecard_hole.rb
@@ -2,6 +2,7 @@ class ScorecardHole < ActiveRecord::Base
   belongs_to :scorecard
   belongs_to :hole
 
-  validates :score, inclusion: { in: (1..9).to_a << nil }
   validates :scorecard, :hole, :par, presence: true
+  validates :score, inclusion: (1..8).to_a << nil
+  validates :par, inclusion: (3..5).to_a
 end
