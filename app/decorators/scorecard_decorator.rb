c805b611ab7d92ff5c623e4892bdd4cae2ace695
Geoff Schorkopf <gschorkopf@gmail.com>
2014-03-09 19:53:52 -0400

    Adds decorator method scorecard#display_score

--- app/decorators/scorecard_decorator.rb
+++ app/decorators/scorecard_decorator.rb
@@ -5,12 +5,16 @@ class ScorecardDecorator < Draper::Decorator
     user.name
   end
 
+  def display_score
+    "#{total} #{shooting}"
+  end
+
+  private
+
   def shooting
     return "(Even)" if at_par?
     return "(#{shooting_symbol}#{source.shooting})"
   end
-
-  private
 
   def shooting_symbol
     "+" if over_par?
