58bd2936fec7fb56e9fcb04d545b5934930d75a4
Geoff Schorkopf <gschorkopf@gmail.com>
2014-03-13 18:04:05 -0400

    Adds rounds_controller#create

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -3,4 +3,20 @@ class RoundsController < ApplicationController
     @round_form = RoundForm.new
   end
 
+  def create
+    @round_form = RoundForm.new(args: round_form_params)
+
+    if @round_form.valid?
+      @round = @round_form.save
+      redirect_to root_path, notice: "New Round Created!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def round_form_params
+    params.require(:round_form).permit(:course_id, user_ids: [])
+  end
 end
--- app/models/round_form.rb
+++ app/models/round_form.rb
@@ -12,8 +12,32 @@ class RoundForm
     @course_id = args["course_id"]
     @round_factory = round_factory
   end
+
+  def valid?
+    course.present? && all_users_found?
+  end
+
+  def save
+    round_factory.create_round(course: course, users: users)
+  end
+
   private
+
+  attr_reader :users, :course
+
   def reject_empty(ids)
     ids.present? && ids.reject(&:empty?)
+  end
+
+  def all_users_found?
+    users.count == user_ids.count
+  end
+
+  def course
+    @course ||= Course.find_by(id: course_id)
+  end
+
+  def users
+    @users ||= User.where(id: user_ids)
   end
 end
