324f3700ac8e809172e5aac6280828bf504bd090
Geoff Schorkopf <gschorkopf@gmail.com>
2014-03-16 22:11:58 -0400

    Adds login/logout paths and views

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_by(email: sign_in_params[:email])
+
+    if @user && @user.authenticate(sign_in_params[:password])
+      sign_in @user
+      # Flash notice
+      redirect_to root_url
+    else
+      # Flash notice
+      redirect_to root_url
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_url
+  end
+
+  private
+
+  def sign_in_params
+    params.require(:session).permit(:email, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Frolfr::Application.routes.draw do
 
   resources :home, only: [:show]
   resources :rounds, only: [:new, :create]
+  resources :sessions, only: [:create, :destroy]
   resources :turns, only: [:show, :update]
 end
