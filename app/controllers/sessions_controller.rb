b108e3aa93e9059517c3e0e9eab5d3096e9b5708
Geoff Schorkopf <gschorkopf@gmail.com>
2014-03-16 23:31:33 -0400

    Abstracts the login logic from the home page and into new routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
   def create
     @user = User.find_by(email: sign_in_params[:email])
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Frolfr::Application.routes.draw do
   root to: 'home#show'
+  get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
 
   resources :home, only: [:show]
   resources :rounds, only: [:new, :create]
-  resources :sessions, only: [:create, :destroy]
+  resources :sessions, only: [:create]
   resources :turns, only: [:show, :update]
 end
