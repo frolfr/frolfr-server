09acb04a031b86713d7bb993fd85a2d2fd9c8a1a
Geoff Schorkopf <gschorkopf@gmail.com>
2014-03-21 18:28:52 -0400

    Adds auth token to users; rakes database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140318022345) do
+ActiveRecord::Schema.define(version: 20140321215646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20140318022345) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "auth_token"
   end
 
 end
