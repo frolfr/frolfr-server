c4d773b77698023734c45e5c9a33702a1ec806fe
Geoff Schorkopf <gschorkopf@gmail.com>
2014-03-28 17:27:04 -0400

    Adds some dummy data for images

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,4 +4,13 @@ class Course < ActiveRecord::Base
 
   validates :state, :city, :country, :name, presence: true
   validates_uniqueness_of :name, scope: :city
+
+  IMAGES = {
+    Course.find_by(name: "Perkerson Park") => "http://www.dgcoursereview.com/course_pics/2590/da413035_m.jpg",
+    Course.find_by(name: "Redan Park") => "http://www.pdga.com/files/styles/large/public/course_photos/REDAN%20SIGN.jpg"
+  }.freeze
+
+  def image
+    IMAGES[self]
+  end
 end
