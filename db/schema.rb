bb5ebbeeefd839334a04763081723a6cca511249
Geoff Schorkopf <gschorkopf@gmail.com>
2014-04-10 17:36:00 -0400

    Creates friendships model; migrates DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140330053903) do
+ActiveRecord::Schema.define(version: 20140410204309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20140330053903) do
     t.string   "state"
     t.string   "country"
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "friendships", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
