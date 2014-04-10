bcae9147c92f42193dfa44e4303947fd7157eb2b
Geoff Schorkopf <gschorkopf@gmail.com>
2014-04-10 18:24:44 -0400

    Adds validation so you cannot add yourself as a friend

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -5,9 +5,16 @@ class Friendship < ActiveRecord::Base
   validates :user, :friend, presence: true
   validates_uniqueness_of :user, scope: :friend
   validates_uniqueness_of :friend, scope: :user
+  validate :cannot_add_self
 
   def self.create_friendship(user: nil, friend: nil)
     create(user: user, friend: friend)
     create(user: friend, friend: user)
+  end
+
+  private
+
+  def cannot_add_self
+    return false if user == friend
   end
 end
