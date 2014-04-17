706efc66a828fbde5edd6b73a1eca27ea4f2c288
Geoff Schorkopf <gschorkopf@gmail.com>
2014-04-16 20:12:49 -0400

    Rips out some chunks of jquery mobile logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :set_locale, :prepare_for_mobile, :require_login
-  helper_method :mobile_device?, :current_user, :logged_in?, :differentiate_path
+  before_action :set_locale, :require_login
+  helper_method :current_user, :logged_in?
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
@@ -13,12 +13,6 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     current_user.present?
-  end
-
-  def differentiate_path(path, *args)
-    attempt = request.parameters["attempt"].to_i + 1
-    args.unshift(path).push(attempt: attempt)
-    send(*args)
   end
 
   private
