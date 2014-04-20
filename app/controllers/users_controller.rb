7ac0ad7f87a71bb0d49d24bf3daa30216f29ad35
Geoff Schorkopf <gschorkopf@gmail.com>
2014-04-20 10:56:23 -0400

    Adds users#new and users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_new_params)
+
+    if @user.save
+      sign_in(@user)
+      redirect_to root_path, notice: t('misc.welcome', user: @user.first_name)
+    else
+      render :new, alert: "There were errors with your submission"
+    end
+  end
+
   def edit
   end
 
@@ -9,7 +26,13 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:first_name, :middle_name, :last_name)
+  def user_edit_params
+    params.require(:user)
+      .permit(:first_name, :middle_name, :last_name)
+  end
+
+  def user_new_params
+    params.require(:user)
+      .permit(:first_name, :middle_name, :last_name, :email, :password, :password_confirmation)
   end
 end
