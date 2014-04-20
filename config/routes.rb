cd7b1034a9be7e4a18778f55539979ebaa664a69
Geoff Schorkopf <gschorkopf@gmail.com>
2014-04-20 10:55:44 -0400

    Adds routes for sign ups

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Frolfr::Application.routes.draw do
   get '/login', to: 'sessions#new'
   get '/logout', to: 'sessions#destroy'
   get '/profile', to: 'users#edit'
+  get '/sign_up', to: 'users#new'
 
   resources :courses, only: [:index, :show]
   resources :rounds, only: [:index, :new, :create, :show]
   resources :sessions, only: [:create]
   resources :turns, only: [:show, :update]
-  resources :users, only: [:update]
+  resources :users, only: [:update, :create]
 end
