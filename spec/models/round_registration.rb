808ed70a1457d74388c27e684f06b1673981014a
Geoff Schorkopf <gschorkopf@gmail.com>
2014-04-25 11:43:13 -0500

    Tidies RoundRegistration spec up

--- spec/models/round_registration.rb
+++ spec/models/round_registration.rb
@@ -2,27 +2,32 @@ require 'spec_helper'
 
 describe RoundRegistration do
   let(:course) { FactoryGirl.create(:course) }
-  let(:users) { [FactoryGirl.create(:user), FactoryGirl.create(:user)] }
+  let(:users) { FactoryGirl.create_list(:user, user_count) }
   subject(:round_registration) { described_class.new(course: course, users: users) }
 
   before do
-    1.upto(3) do |i|
+    hole_count.times do
       FactoryGirl.create(:hole, course: course)
     end
   end
 
+  let(:user_count) { 2 }
+  let(:hole_count) { 3 }
+
   context "a two player, three hole course" do
     describe "#create_round" do
-      it "creates as six turns" do
+      it "creates six turns" do
+        turn_count = user_count * hole_count
+
         expect {
           subject.create_round
-        }.to change(Turn, :count).by(6)
+        }.to change(Turn, :count).by(turn_count)
       end
 
       it "creates two scorecards" do
         expect {
           subject.create_round
-        }.to change(Scorecard, :count).by(2)
+        }.to change(Scorecard, :count).by(user_count)
       end
 
       it "creates one round" do
