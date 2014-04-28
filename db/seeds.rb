ee7a1d46ecf6616ea0eb7f298aad793a6342802c
Geoff Schorkopf <gschorkopf@gmail.com>
2014-04-27 22:56:36 -0400

    Removes unused seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,26 +13,10 @@ course1 = Course.create(
   country: "United States"
 )
 
-course2 = Course.create(
-  name: "Redan Park",
-  city: "Lithonia",
-  state: "GA",
-  country: "United States"
-)
-
 FIRST_HOLE = 1
 TOTAL_HOLES = 18
 
-FIRST_HOLE.upto(TOTAL_HOLES) do |i| 
+FIRST_HOLE.upto(TOTAL_HOLES) do |i|
   Hole.create(number: i, course: course1)
   Hole.create(number: i, course: course2)
 end
-
-users = User.create([
-  { name: "Geoff", email: "geoff@frolfr.com" },
-  { name: "Jenna", email: "jenna@frolfr.com" },
-  { name: "Hyman", email: "hyman@frolfr.com" },
-  { name: "Jason", email: "jason@frolfr.com" },
-  { name: "Felix", email: "felix@frolfr.com" },
-  { name: "Phillips", email: "phillips@frolfr.com" }
-])
