d498d6d47285ffb46a4034c2eab84762b121347d
Geoff Schorkopf <gschorkopf@gmail.com>
2014-07-08 10:11:17 -0400

    Builds out dashboard with facade pattern

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   def show
     scorecard = current_user.recently_completed_scorecard
     @scorecard = scorecard.decorate if scorecard
+    @dashboard = Dashboard.new(current_user).decorate
   end
 end
---
+++ app/models/dashboard.rb
@@ -0,0 +1,8 @@
+class Dashboard
+  attr_reader :user
+
+  def initialize(user)
+    @user = user
+  end
+
+end
