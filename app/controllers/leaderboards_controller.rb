3423b66e93af94083a7e94ea4cde10c5c5a9e23f
Geoff Schorkopf <gschorkopf@gmail.com>
2014-07-20 12:12:46 -0400

    Adds some touches to leaderboards#show

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -1,8 +1,8 @@
 class LeaderboardsController < ApplicationController
   def show
-    course = Course.find(params[:course_id]).decorate
+    @course = Course.find(params[:course_id]).decorate
 
-    scorecards = course.scorecards.by_shooting
+    scorecards = @course.scorecards.by_shooting
     best_scorecards = scorecards.select(&:completed?)
     @scorecards = ScorecardDecorator.decorate_collection(best_scorecards[0..9])
     # TODO: Clean up by_shooting, limiting
