f88de35840812a20a118e048066f973b378df63c
Geoff Schorkopf <gschorkopf@gmail.com>
2014-09-10 19:22:26 -0400

    Removes unused method

--- app/models/turn.rb
+++ app/models/turn.rb
@@ -15,11 +15,6 @@ class Turn < ActiveRecord::Base
     score.present?
   end
 
-  def update_all_pars(par)
-    shs = round.scorecards.map { |scorecard| scorecard.turn_for_hole(hole.number) }
-    shs.each { |sh| sh.update(par: par) }
-  end
-
   def round
     scorecard.round
   end
