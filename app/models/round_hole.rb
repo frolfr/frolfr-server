fad69888f3eeaeacee4182c763b17eab46033ced
Geoff Schorkopf <gschorkopf@gmail.com>
2014-09-10 19:22:18 -0400

    Writes RoundHole class

---
+++ app/models/round_hole.rb
@@ -0,0 +1,22 @@
+class RoundHole
+  attr_reader :round, :hole, :turns
+
+  def initialize(round:, hole:)
+    @round = round
+    @hole = hole
+  end
+
+  def turns
+    @turns ||= round.turns_for_hole(hole)
+  end
+
+  def update(params)
+    par = params[:par]
+    results = params[:results]
+
+    turns.each do |turn|
+      score = results[turn.id]
+      turn.update(par: par, score: score)
+    end
+  end
+end
