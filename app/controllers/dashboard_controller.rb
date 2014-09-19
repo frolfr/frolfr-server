f23c9a42d90631108d0dc04d90992b037da4f262
Geoff Schorkopf <gschorkopf@gmail.com>
2014-09-19 12:49:05 -0500

    Removes unused Dashboard logic

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,5 @@ class DashboardController < ApplicationController
   def show
     scorecard = current_user.recently_completed_scorecard
     @scorecard = scorecard.decorate if scorecard
-    @dashboard = Dashboard.new(current_user).decorate
   end
 end
--- app/decorators/dashboard_decorator.rb
+++
@@ -1,3 +0,0 @@
-class DashboardDecorator < Draper::Decorator
-  delegate_all
-end
