b661711a030244eeb2d19da34a0906e07a4071e0
Jordan Killpack <jordan@bignerdranch.com>
2014-09-30 16:02:50 -0400

    Add course serializer
    [#13]

---
+++ app/serializers/course_serializer.rb
@@ -0,0 +1,17 @@
+class CourseSerializer < ActiveModel::Serializer
+  attributes :id, :city, :state, :country, :name, :status, :location, :rounds_played
+
+  def location
+    "#{city}, #{state}"
+  end
+
+  def rounds_played
+    user_scorecards.count
+  end
+
+  private
+
+  def user_scorecards
+    scope.scorecards_for_course(object)
+  end
+end
