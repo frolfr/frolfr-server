7921c16db242b1eaec770f193329e464a28a4bc0
Geoff Schorkopf <gschorkopf@gmail.com>
2014-10-07 17:39:15 -0400

    Updates syntax

--- app/controllers/api/rounds_controller.rb
+++ app/controllers/api/rounds_controller.rb
@@ -3,7 +3,7 @@ class Api::RoundsController < ActionController::Base
   respond_to :json
 
   def show
-    round = Round.find params[:id]
+    round = Round.find(params[:id])
     respond_with round
   end
 
