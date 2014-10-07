c8ff4123f821d22c9e3abfbd2063ac1f840d946d
Geoff Schorkopf <gschorkopf@gmail.com>
2014-10-07 17:36:51 -0400

    Adds necessary scorecard_ids to RoundSerializer
    [#14]

--- app/serializers/round_serializer.rb
+++ app/serializers/round_serializer.rb
@@ -1,7 +1,11 @@
 class RoundSerializer < ActiveModel::Serializer
-  attributes :id, :course_name, :created_at, :marked_complete
+  attributes :id, :course_name, :created_at, :marked_complete, :scorecard_ids
 
   def course_name
     object.course.name
+  end
+
+  def scorecard_ids
+    object.scorecards.pluck(:id)
   end
 end
