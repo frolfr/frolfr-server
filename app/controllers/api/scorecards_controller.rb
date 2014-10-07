472bec3be52573ee66aa4bd13c2bb689d957c4c4
Geoff Schorkopf <gschorkopf@gmail.com>
2014-10-07 17:39:15 -0400

    Updates syntax

--- app/controllers/api/scorecards_controller.rb
+++ app/controllers/api/scorecards_controller.rb
@@ -3,7 +3,7 @@ class Api::ScorecardsController < ActionController::Base
   respond_to :json
 
   def show
-    scorecard = Scorecard.find params[:id]
+    scorecard = Scorecard.find(params[:id])
     respond_with scorecard
   end
 end
