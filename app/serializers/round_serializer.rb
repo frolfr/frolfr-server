341f53472a06bf1c537ef686db8361297c1bac7f
Patrick Dougall <pdougall1@gmail.com>
2014-10-09 18:29:45 -0400

    flesh out: round resource

--- app/serializers/round_serializer.rb
+++ app/serializers/round_serializer.rb
@@ -1,8 +1,12 @@
 class RoundSerializer < ActiveModel::Serializer
-  attributes :id, :course_name, :created_at, :marked_complete, :scorecard_ids
+  attributes :id, :course_name, :created_at, :marked_complete, :scorecard_ids, :course_id
 
   def course_name
     object.course.name
+  end
+
+  def course_id
+    object.course.id
   end
 
   def scorecard_ids
