53f0090437d64ddd93cf016d71d242f0dadbb944
Patrick Dougall <pdougall1@gmail.com>
2014-10-09 18:29:45 -0400

    flesh out: round resource

--- app/assets/javascripts/models/round.js
+++ app/assets/javascripts/models/round.js
@@ -2,5 +2,7 @@ App.Round = DS.Model.extend({
   courseName: DS.attr('string'),
   createdAt: DS.attr('date'),
   markedComplete: DS.attr('boolean'),
-  scorecards: DS.hasMany('scorecard', {async: true})
+  courseId: DS.attr('number'),
+  scorecards: DS.hasMany('scorecard', {async: true}),
+  course: DS.belongsTo('course', {async: true})
 });
