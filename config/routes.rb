9578337b88fb0e93fbdf24e487c525bfd36aec10
Geoff Schorkopf <gschorkopf@gmail.com>
2014-10-24 16:21:26 -0400

    Writes current_users#show action
    [#31]

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Frolfr::Application.routes.draw do
   namespace :api do
     resources :authorizations
     resources :courses
+    resource :current_user, only: [:show]
   end
 
   get '*path', to: 'dashboard#show'
