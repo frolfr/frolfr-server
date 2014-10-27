cf6138344fe42742b91de33949f50e160adb0fb4
Jordan Killpack <jordan@bignerdranch.com>
2014-10-27 17:11:23 -0400

    Show current user information if authenticated and so forth
    [#31]

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -2,5 +2,8 @@ App.User = DS.Model.extend({
     firstName: DS.attr('string'),
     middleName: DS.attr('string'),
     lastName: DS.attr('string'),
-    email: DS.attr('string')
+    email: DS.attr('string'),
+    fullName: function() {
+      return this.get('firstName') + " " + this.get('lastName');
+    }.property("firstName", "lastName")
 });
