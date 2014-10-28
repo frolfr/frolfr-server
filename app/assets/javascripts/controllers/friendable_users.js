5c2089b20bc6d750c47c81fc75a14c9580762ad6
Jordan Killpack <jordan@bignerdranch.com>
2014-10-28 12:28:10 -0400

    Add a friendable users controller; embed it in the friends route
    Also add a little crappy placeholder friendable users template.
    
    [#22]

---
+++ app/assets/javascripts/controllers/friendable_users.js
@@ -0,0 +1,1 @@
+App.FriendableUsersController = Ember.ArrayController.extend();
--- app/assets/javascripts/routes/friends.js
+++ app/assets/javascripts/routes/friends.js
@@ -1,5 +1,10 @@
 App.FriendsRoute = App.AuthenticatedRoute.extend({
   model: function() {
     return this.store.find('friend');
+  },
+
+  setupController: function(controller, model) {
+    this._super();
+    this.controllerFor('friendableUsers').set('model', this.store.find('friendableUser'));
   }
 });
