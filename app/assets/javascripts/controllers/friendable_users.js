9c1cdb0c0d1dc4a556b792fe584e5632e4efc373
Jordan Killpack <jordan@bignerdranch.com>
2014-10-28 18:22:42 -0400

    Add autocomplete field to friendable users page
    [#22]

--- app/assets/javascripts/controllers/friendable_users.js
+++ app/assets/javascripts/controllers/friendable_users.js
@@ -1,1 +1,34 @@
-App.FriendableUsersController = Ember.ArrayController.extend();
+App.FriendableUsersController = Ember.ArrayController.extend({
+  search: "",
+  user: null,
+
+  searchResults: function() {
+    var search = this.get('search').toLowerCase();
+    return this.filter(function(user) {
+      return search !== "" && user.get('fullName').toLowerCase().indexOf(search) != -1;
+    });
+  }.property('search', 'this.@each.fullName'),
+
+  actions: {
+    addFriend: function() {
+      console.log("NOT IMPLEMENTED!!!");
+      console.log("you're gonna be friends with ", this.get('user.fullName'));
+    }
+  },
+
+  // stuff for AutoCompleteComponent
+
+  processQuery: function(query, callback) {
+    this.set('search', query);
+    callback(this.get('searchResults'));
+  },
+
+  formatSuggestion: function(user) {
+    return user.get('fullName');
+  },
+
+  setSuggestion: function(suggestion) {
+    this.set('user', suggestion);
+  }
+
+});
