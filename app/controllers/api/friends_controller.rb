821c78861973b37ee839b0d4d85a42b5f183b355
Jordan Killpack <jordan@bignerdranch.com>
2014-10-29 11:45:43 -0400

    Add an endpoint for creating a new friend
    [#22]

--- app/controllers/api/friends_controller.rb
+++ app/controllers/api/friends_controller.rb
@@ -4,4 +4,16 @@ class Api::FriendsController < ApplicationController
   def index
     respond_with current_user.friends
   end
+
+  def create
+    new_friend = User.find(friend_params[:id])
+    current_user.friends << new_friend
+    respond_with new_friend, location: api_friend_url(new_friend), root: "friend"
+  end
+
+  private
+
+  def friend_params
+    params.require(:friend).permit(:id)
+  end
 end
