bdf689a89e94feebabe2b33b69172a44f711d214
Jordan Killpack <jordan@bignerdranch.com>
2014-10-29 10:55:33 -0400

    Clean up autocomplete component + friendable users controller
    - Make it leverage events and properties, rather than a bunch of custom
      functions and stuff
    - Fix clearing out the old user if the query has changed
    - Disable the + button if there's no user selected
    
    [#22]

--- app/assets/javascripts/components/autocomplete.js
+++ app/assets/javascripts/components/autocomplete.js
@@ -1,37 +1,43 @@
 App.AutoCompleteComponent = Ember.TextField.extend({
   // This component won't work without a `source`.
-  // Sources need to implement three methods:
-  // - processQuery(query, callback)
-  //    This should retrieve a list of suggestions based on
-  //    `query`, then invoke `callback` on the list.
   //
-  // - formatSuggestion(suggestion)
-  //    This should return a displayable string for the given
-  //    suggestion.
+  // Sources need to have two properties:
+  //   - query
+  //   - results
+  // When a user types into the autocomplete field, the component
+  // will update the source's `query` property, then ask the source
+  // for `results`. It is up to the source to compute results based
+  // on the given query.
   //
-  // - setSuggestion(suggestion)
-  //    This is invoked when a suggestion is selected.
-
+  // Additionally, the source should implement the following method:
+  //   - formatSuggestion(suggestion)
+  //     This should return a displayable string for the given
+  //     suggestion.
+  //
   didInsertElement: function() {
     this._super();
     var $input = this.$();
     var _this = this;
-    $input.typeahead(null, {
+    $input.typeahead({
+      highlight: true,
+      hint: false
+    }, {
       displayKey: this.get('source').formatSuggestion,
       source: function(query, callback) {
-        _this.get('source').processQuery(query, callback);
+        // Normally we could just bind this.value to source.query.
+        // But we need to make sure that the value gets propogated
+        // to the source *before* we ask for results, hence this
+        // manual `set` call.
+        _this.set("source.query", query);
+
+        var results = _this.get('source.results');
+        callback(results);
       }
     });
 
-    var onComplete = function(event, suggestion) {
-      _this.get('source').setSuggestion(suggestion);
-    };
-
-    $input.on('typeahead:autocompleted', onComplete);
-    $input.on('typeahead:selected', onComplete);
-    $input.on('keydown', function() {
-      _this.get('source').setSuggestion(null);
+    $input.on('typeahead:selected', function(event, suggestion) {
+      _this.sendAction('gotSuggestion', suggestion);
     });
-    //
   }
+
 });
--- app/assets/javascripts/controllers/friendable_users.js
+++ app/assets/javascripts/controllers/friendable_users.js
@@ -1,34 +1,43 @@
 App.FriendableUsersController = Ember.ArrayController.extend({
-  search: "",
+  query: "",
   user: null,
 
-  searchResults: function() {
-    var search = this.get('search').toLowerCase();
+  results: function() {
+    var query = this.get('query').toLowerCase();
     return this.filter(function(user) {
-      return search !== "" && user.get('fullName').toLowerCase().indexOf(search) != -1;
+      return query !== "" && user.get('fullName').toLowerCase().indexOf(query) != -1;
     });
-  }.property('search', 'this.@each.fullName'),
+  }.property('query', 'model'),
+
+  hasNoUser: function() {
+    return Ember.isEmpty(this.get('user.id'));
+  }.property('user'),
+
+  clearStaleUser: function() {
+    if (this.get('hasNoUser')) {
+      return;
+    }  else if (this.get('query') !== this.formatSuggestion(this.get('user'))) {
+      this.set('user', null);
+    }
+  }.observes("query", "user"),
 
   actions: {
+    updateFriendField: function(user) {
+      this.set('user', user);
+    },
+
     addFriend: function() {
       console.log("NOT IMPLEMENTED!!!");
       console.log("you're gonna be friends with ", this.get('user.fullName'));
+      this.set('user', null);
+      this.set('query', "");
     }
   },
 
   // stuff for AutoCompleteComponent
 
-  processQuery: function(query, callback) {
-    this.set('search', query);
-    callback(this.get('searchResults'));
-  },
-
   formatSuggestion: function(user) {
     return user.get('fullName');
   },
-
-  setSuggestion: function(suggestion) {
-    this.set('user', suggestion);
-  }
 
 });
