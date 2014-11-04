be87b5f56299dd74b7ff1abcd665074f02712eaa
Geoff Schorkopf <gschorkopf@gmail.com>
2014-11-04 12:13:45 -0500

    Cleans up the spec

--- spec/steps/signing_up_steps.rb
+++ spec/steps/signing_up_steps.rb
@@ -1,17 +1,17 @@
-step "I visit the sign up page" do
+step 'I visit the sign up page' do
   visit '/users/new'
 end
 
-step "I fill out the sign up form" do
-  page.fill_in 'email', with: "david@frolfr.com"
-  page.fill_in 'firstName', with: @first_name = "David"
-  page.fill_in 'lastName', with: "Bowie"
-  page.fill_in 'password', with: "password"
-  page.fill_in 'passwordConfirmation', with: "password"
+step 'I fill out the sign up form' do
+  page.fill_in 'email', with: 'david@frolfr.com'
+  page.fill_in 'firstName', with: @first_name = 'David'
+  page.fill_in 'lastName', with: @last_name = 'Bowie'
+  page.fill_in 'password', with: 'password'
+  page.fill_in 'passwordConfirmation', with: 'password'
   page.click_on 'Sign Up'
 end
 
-step "I see I have signed up" do
+step 'I see I have signed up' do
   expect(page).to have_content 'Welcome to Frolfr!'
-  expect(page).to have_content 'David Bowie'
+  expect(page).to have_content "#{@first_name} #{@last_name}"
 end
