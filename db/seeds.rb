199be29a76449c1db23539ba9c2de5ec1d9315b2
Geoff Schorkopf <gschorkopf@gmail.com>
2014-11-05 14:27:39 -0500

    Adds more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 User.destroy_all
 Course.destroy_all
 Round.destroy_all
 
 User.create(
-  first_name: 'Frolfr',
-  last_name: 'Champ',
+  first_name: 'Jordan',
+  last_name: 'Killpack',
   password: 'password',
-  email: 'champ@frolfr.com',
+  email: 'jordan@frolfr.com',
   password_confirmation: 'password'
 )
 
 User.create(
-  first_name: 'New',
-  last_name: 'Discr',
+  first_name: 'Don',
+  last_name: 'Pottinger',
   password: 'password',
-  email: 'newb@frolfr.com',
+  email: 'don@frolfr.com',
   password_confirmation: 'password'
 )
+
+User.create(
+  first_name: 'Patrick',
+  last_name: 'Dougall',
+  password: 'password',
+  email: 'patrick@frolfr.com',
+  password_confirmation: 'password'
+)
+
+User.create(
+  first_name: 'Sebastian',
+  last_name: 'Geiger',
+  password: 'password',
+  email: 'sebastian@frolfr.com',
+  password_confirmation: 'password'
+)
+
+User.all.each do |user|
+  User.all.each do |friend|
+    Friendship.create_friendship(user: user, friend: friend)
+  end
+end
 
 course = Course.create(
   name: "Perkerson Park",
@@ -33,28 +47,52 @@ course = Course.create(
   country: "United States"
 )
 
-1.upto(18) do |number|
-  course.holes.create(number: number)
-end
-
-round = Round.create(
-  course: course,
-  marked_complete: true
+course2 = Course.create(
+  name: "Redan Park",
+  city: "Lithonia",
+  state: "GA",
+  country: "United States"
 )
 
-User.all.each do |user|
-  Scorecard.create(
-    round: round,
-    user: user
-  )
+1.upto(18) do |number|
+  course.holes.create(number: number)
+  course2.holes.create(number: number)
 end
 
-Scorecard.all.each do |scorecard|
-  course.holes.each do |hole|
-    scorecard.turns.create(
-      hole: hole,
-      score: (2..6).to_a.sample
+5.times do
+  round = Round.create(
+    course: course,
+    marked_complete: true
+  )
+  User.all.each do |user|
+    scorecard = Scorecard.create(
+      round: round,
+      user: user
     )
+    course.holes.each do |hole|
+      scorecard.turns.create(
+        hole: hole,
+        score: (2..4).to_a.sample
+      )
+    end
   end
 end
 
+5.times do
+  round = Round.create(
+    course: course2,
+    marked_complete: true
+  )
+  User.all.each do |user|
+    scorecard = Scorecard.create(
+      round: round,
+      user: user
+    )
+    course2.holes.each do |hole|
+      scorecard.turns.create(
+        hole: hole,
+        score: (2..4).to_a.sample
+      )
+    end
+  end
+end
