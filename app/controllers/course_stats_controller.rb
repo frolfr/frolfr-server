71148e74d43625e10631acb9bd84bd242683bc99
Geoff Schorkopf <gschorkopf@gmail.com>
2014-11-11 15:27:01 -0500

    Removes a bunch of files we don't use

--- app/controllers/course_stats_controller.rb
+++
@@ -1,7 +0,0 @@
-class CourseStatsController < ApplicationController
-  def show
-    @course = Course.find(params[:id]).decorate
-    @course_stats = CourseStats.new(course: @course.object, user: current_user)
-    @holes_stats = @course_stats.holes_stats
-  end
-end
--- app/inputs/fake_input.rb
+++
@@ -1,5 +0,0 @@
-class FakeInput < SimpleForm::Inputs::StringInput
-  def input(wrapper_options)
-    template.text_field_tag(attribute_name, nil, wrapper_options)
-  end
-end
