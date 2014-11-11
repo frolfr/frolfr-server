890eddda3146c51459003b460be604fe10c787a1
Patrick Dougall <pdougall1@gmail.com>
2014-11-11 16:55:48 -0500

    adds dynamic properties for scores and par

--- app/assets/javascripts/models/turn.js
+++ app/assets/javascripts/models/turn.js
@@ -2,5 +2,22 @@ App.Turn = DS.Model.extend({
   score: DS.attr('number'),
   par: DS.attr('number'),
   holeNumber: DS.attr('string'),
-  scorecard: DS.belongsTo('scorecard')
+  scorecard: DS.belongsTo('scorecard'),
+
+  parStatus: function () {
+    var parScore = this.get('parScore');
+    if (parScore < 0) {
+      return 'belowPar'
+    } else if (parScore == 0) {
+      return 'atPar'
+    } else if (parScore > 0) {
+      return 'abovePar'
+    }
+  }.property('parScore'),
+
+  parScore: function () {
+    return this.get('score') - this.get('par');
+  }.property('score', 'par')
+
+
 });
