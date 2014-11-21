f9d41f54fb615985b4ade98c4866865ffe946020
Geoff Schorkopf <gschorkopf@gmail.com>
2014-11-21 16:14:17 -0500

    Adds icon arrows to sortability

--- app/assets/javascripts/controllers/rounds_controller.js
+++ app/assets/javascripts/controllers/rounds_controller.js
@@ -2,9 +2,45 @@ App.RoundsController = Ember.ArrayController.extend({
   sortProperties: ['createdAt'],
   sortAscending: false,
 
+  currentSortProperty: function() {
+    return this.get('sortProperties').get('0');
+  }.property('sortProperties'),
+
+  sortCourseName: function() {
+    if (this.get('currentSortProperty') === 'courseName') {
+      return this.get('sortDirection');
+    } else {
+      return '';
+    }
+  }.property('currentSortProperty', 'sortDirection'),
+
+  sortCreatedAt: function() {
+    if (this.get('currentSortProperty') === 'createdAt') {
+      return this.get('sortDirection');
+    } else {
+      return '';
+    }
+  }.property('currentSortProperty', 'sortDirection'),
+
+  sortTotalScore: function() {
+    if (this.get('currentSortProperty') === 'totalScore') {
+      return this.get('sortDirection');
+    } else {
+      return '';
+    }
+  }.property('currentSortProperty', 'sortDirection'),
+
+  sortDirection: function() {
+    if (this.get('sortAscending')) {
+      return 'fi fi-arrow-up';
+    } else {
+      return 'fi fi-arrow-down';
+    }
+  }.property('sortAscending'),
+
   actions: {
     sortBy: function(property) {
-      isAscending = !this.get('sortAscending');
+      var isAscending = !this.get('sortAscending');
 
       this.set('sortProperties', [property]);
       this.set('sortAscending', isAscending);
