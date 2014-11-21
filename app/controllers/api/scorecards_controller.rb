f0378ba2f08c4fa8f4401f51d1d348393bd71b2d
Geoff Schorkopf <gschorkopf@gmail.com>
2014-11-21 16:51:09 -0500

    Removes unused controller action, scorecards#index

--- app/controllers/api/scorecards_controller.rb
+++ app/controllers/api/scorecards_controller.rb
@@ -1,11 +1,6 @@
 class Api::ScorecardsController < ApplicationController
   respond_to :json
 
-  def index
-    scorecards = current_user.scorecards_by_date.limit(10)
-    respond_with scorecards
-  end
-
   def show
     scorecard = Scorecard.find(params[:id])
     respond_with scorecard
