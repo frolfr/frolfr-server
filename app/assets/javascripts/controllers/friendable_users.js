4e8266b4950115ef5f89e9362322d0f1009e90ad
Jordan Killpack <jordan@bignerdranch.com>
2014-11-21 13:50:38 -0500

    Rework autocomplete component so it sucks less
    [Closes #51]

--- app/assets/javascripts/controllers/friendable_users.js
+++ app/assets/javascripts/controllers/friendable_users.js
@@ -1,57 +1,31 @@
 App.FriendableUsersController = Ember.ArrayController.extend({
-  query: "",
-  selectedUser: null,
-
-  results: function() {
-    var query = this.get('query').toLowerCase();
-    return this.filter(function(user) {
-      return query !== "" && user.get('fullName').toLowerCase().indexOf(query) != -1;
-    });
-  }.property('query', 'model'),
+  selection: null,
+  query: null,
 
   noUserSelected: function() {
-    return Ember.isEmpty(this.get('selectedUser.id'));
-  }.property('selectedUser'),
-
-  clearStaleUser: function() {
-    if (this.get('noUserSelected')) {
-      return;
-    }  else if (this.get('query') !== this.formatSuggestion(this.get('selectedUser'))) {
-      this.set('selectedUser', null);
-    }
-  }.observes("query", "selectedUser"),
+    return Ember.isEmpty(this.get('selection'));
+  }.property('selection'),
 
   actions: {
-    updateSelectedUser: function(user) {
-      this.set('selectedUser', user);
-    },
-
     addFriend: function() {
       var _this = this;
 
       var newFriend = this.store.createRecord('friend',
-        this.get("selectedUser").toJSON({includeId: true})
+        this.get("selection").toJSON({includeId: true})
       );
 
       newFriend.save().then(function(savedFriend) {
         // This user is now our friend, so remove them from the list of friendable users
-        var selectedUser = _this.store.find('friendableUser', _this.get('selectedUser.id'));
+        var selectedUser = _this.store.find('friendableUser', _this.get('selection.id'));
         selectedUser.then(function(selectedUser) {
           selectedUser.deleteRecord();
         });
 
         // Clear out the form
-        _this.set('selectedUser', null);
-        _this.set('query', "");
+        _this.set('selection', null);
+        _this.set('query', null);
       });
 
     }
-  },
-
-  // stuff for AutoCompleteComponent
-
-  formatSuggestion: function(user) {
-    return user.get('fullName');
-  },
-
+  }
 });
