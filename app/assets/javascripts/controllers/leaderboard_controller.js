7f0f66549ce8c347e211a7f97fa9bdac26981f77
Geoff Schorkopf <gschorkopf@gmail.com>
2014-12-11 16:11:07 -0500

    Ups leaderboard total to 10

--- app/assets/javascripts/controllers/leaderboard_controller.js
+++ app/assets/javascripts/controllers/leaderboard_controller.js
@@ -1,7 +1,8 @@
 App.LeaderboardController = Ember.ArrayController.extend({
   sortProperties: ['createdAt'],
   sortAscending: false,
+
   filtered: function() {
-    return this.get('content').slice(0,9);
+    return this.get('content').slice(0,10);
   }.property('content')
 })
