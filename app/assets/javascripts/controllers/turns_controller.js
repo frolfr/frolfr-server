596de57c81b37a8795974994b870d74472c5bdc5
Geoff Schorkopf <gschorkopf@gmail.com>
2014-12-12 15:00:36 -0500

    First lazy crack at navigation actions
    A very bad way to do this, to be hopefully fixed in the next commit by
    nesting our resources

--- app/assets/javascripts/controllers/turns_controller.js
+++ app/assets/javascripts/controllers/turns_controller.js
@@ -1,5 +1,30 @@
 App.TurnsController = Ember.ArrayController.extend({
   holeNumber: function() {
     return this.get('model.firstObject.holeNumber');
-  }.property('model.firstObject.holeNumber')
+  }.property('model.firstObject.holeNumber'),
+
+  isFirstHole: function() {
+    return this.get('holeNumber') === "1";
+  }.property('holeNumber'),
+
+  isLastHole: function() {
+    return this.get('holeNumber') === "18";
+    // This should be more intelligent
+  }.property('holeNumber'),
+
+  actions: {
+    previousHole: function() {
+      var hole = parseInt(this.get('holeNumber')) - 1,
+          route = '/rounds/' + 56 + '/holes/' + hole;
+
+      this.transitionToRoute(route);
+    },
+
+    nextHole: function() {
+      var hole = parseInt(this.get('holeNumber')) + 1,
+          route = '/rounds/' + 56 + '/holes/' + hole;
+
+      this.transitionToRoute(route);
+    }
+  }
 })
