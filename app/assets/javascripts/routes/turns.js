e9bacb7d8aee08d37a3e48e840eaef96db96e54d
Geoff Schorkopf <gschorkopf@gmail.com>
2014-12-12 15:28:58 -0500

    Uses Ember nesting to refactor / reuse round routes

--- app/assets/javascripts/routes/turns.js
+++ app/assets/javascripts/routes/turns.js
@@ -1,7 +1,10 @@
 App.TurnsRoute = App.AuthenticatedRoute.extend({
   model: function(params) {
+    var holeNumber = params.hole_number,
+        roundId = this.modelFor('round').id;
+
     return this.store.find('turn', {
-      round_id: params.round_id, hole_number: params.hole_number
+      round_id: roundId, hole_number: holeNumber
     });
   }
 });
