f298dce65a0b13707dc2ccedf3ea1213f90262e0
Geoff Schorkopf <gschorkopf@gmail.com>
2014-12-22 15:21:13 -0500

    Implements global loading action

--- app/assets/javascripts/routes/application.js
+++ app/assets/javascripts/routes/application.js
@@ -3,6 +3,17 @@ App.ApplicationRoute = Ember.Route.extend({
     logout: function() {
       this.controllerFor('sessions').reset();
       App.reset();
+    },
+
+    loading: function() {
+      var view = Ember.View.create({
+        templateName: 'views/spinner',
+        elementId: 'spinner'
+      }).append();
+
+      this.router.one('didTransition', function() {
+        view.destroy();
+      });
     }
   }
 });
