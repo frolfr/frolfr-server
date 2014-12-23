90cfeb3dc395e97bac18170ef9b1638362f39bd4
Jordan Killpack <jordan@bignerdranch.com>
2014-12-23 18:07:01 -0500

    Hold the spinner on the friend page until joint rounds are done loading
    [#85]

--- app/assets/javascripts/routes/user.js
+++ app/assets/javascripts/routes/user.js
@@ -1,10 +1,17 @@
 App.UserRoute = App.AuthenticatedRoute.extend({
   model: function(params) {
-    return this.store.find('user', params.id)
+    return this.store.find('user', params.id);
+  },
+
+  afterModel: function(model, transition) {
+    var _this = this;
+    return this.store.find('jointRound', { user_id: model.id }).then(function(jointRounds) {
+      _this.set('jointRounds', jointRounds);
+    });
   },
 
   setupController: function(controller, model) {
     this._super(controller, model);
-    controller.set('jointRounds', this.store.find('jointRound', { user_id: model.id }));
+    controller.set('jointRounds', this.get('jointRounds'));
   }
 });
