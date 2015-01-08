49f6672fc32316897759e3e381a8f65e3f618de7
Patrick Dougall <pdougall1@gmail.com>
2015-01-07 19:39:20 -0500

    WIP problems with reviews controller
    I keep getting `ActionController::UnknownFormat` for line 17 in the
    `ReviewsController`

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,8 +1,20 @@
 class Api::ReviewsController < ApplicationController
+  respond_to :json
+  skip_before_action :authenticate
 
   def create
-    review = Review.create(review_params)
-    render json: review
+    review = Review.new(review_params)
+
+    if review.save
+      respond_with review
+    else
+      head :unprocessable_entity
+    end
+  end
+
+  def show
+    review = Review.find(params[:id])
+    respond_with review
   end
 
   private
