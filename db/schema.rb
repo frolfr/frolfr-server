53be6dbba4a3393e158ef834d6e397f481892a9f
Geoff Schorkopf <gschorkopf@gmail.com>
2015-01-12 14:23:23 -0500

    Adds password reset columns to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150106194016) do
+ActiveRecord::Schema.define(version: 20150112182656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,8 @@ ActiveRecord::Schema.define(version: 20150106194016) do
     t.string   "last_name"
     t.string   "middle_name"
     t.string   "avatar_url"
+    t.string   "password_reset_token"
+    t.datetime "password_reset_sent_at"
   end
 
 end
