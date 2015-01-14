49c5318621a64476040f3ecf7a09e58e0a390919
Geoff Schorkopf <gschorkopf@gmail.com>
2015-01-14 16:01:06 -0500

    Excludes dirty reviews from reviews index route

--- app/assets/javascripts/routes/reviews_index.js
+++ app/assets/javascripts/routes/reviews_index.js
@@ -1,5 +1,7 @@
 App.ReviewsIndexRoute = App.AuthenticatedRoute.extend({
   model: function() {
-    return this.modelFor('course').get('reviews');
+    return this.modelFor('course').get('reviews').filter(function(review) {
+      return !review.get('isDirty');
+    });
   }
 });
