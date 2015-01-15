f2f2ca422dfbf91e22ec641c5dc4c92a7cab7472
Geoff Schorkopf <gschorkopf@gmail.com>
2015-01-15 12:51:14 -0500

    Adds async calls to review.js

--- app/assets/javascripts/models/review.js
+++ app/assets/javascripts/models/review.js
@@ -1,8 +1,8 @@
 App.Review = DS.Model.extend({
   rating: DS.attr('number'),
   post: DS.attr('string'),
-  course: DS.belongsTo('course'),
-  user: DS.belongsTo('user'),
+  course: DS.belongsTo('course', { async: true }),
+  user: DS.belongsTo('user', { async: true }),
   createdAt: DS.attr('date'),
 
   userFullName: Ember.computed.alias('user.fullName')
