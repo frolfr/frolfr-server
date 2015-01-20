8f951ccb2bb65554e60e6f293ae5667cbd45c13c
Patrick Dougall <pdougall1@gmail.com>
2015-01-20 16:39:04 -0500

    hide new review link if review already made

--- app/assets/javascripts/controllers/reviews_index_controller.js
+++ app/assets/javascripts/controllers/reviews_index_controller.js
@@ -1,4 +1,15 @@
 App.ReviewsIndexController = Ember.ArrayController.extend({
+  needs: ['currentUser'],
+  currentUser: Ember.computed.alias('controllers.currentUser'),
+
   sortProperties: ['createdAt'],
-  sortAscending: false
+  sortAscending: false,
+
+  isAlreadyReviewed: function () {
+    var currentUserId = this.get('currentUser.id'),
+        currentUserReview = this.get('content').findBy('user.id', currentUserId);
+
+    return !!currentUserReview;
+  }.property('content')
+
 });
