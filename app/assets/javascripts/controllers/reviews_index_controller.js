8e741e952c7132a10bfc364aca37efa0f81bea8e
Geoff Schorkopf <gschorkopf@gmail.com>
2015-01-21 16:34:18 -0500

    Adds averageRating to reviews index controller

--- app/assets/javascripts/controllers/reviews_index_controller.js
+++ app/assets/javascripts/controllers/reviews_index_controller.js
@@ -16,6 +16,18 @@ App.ReviewsIndexController = Ember.ArrayController.extend({
         currentUserReview = this.get('content').findBy('user.id', currentUserId);
 
     return !!currentUserReview;
-  }.property('content')
+  }.property('content'),
 
+  averageRating: function() {
+    var sumOfRatings = this.get('filteredReviews').reduce(function (total, review) {
+          return total + review.get('rating');
+        }, 0),
+        reviewsCount = this.get('filteredReviews').length
+
+    return sumOfRatings / reviewsCount;
+  }.property('filteredReviews.@each.rating'),
+
+  averageRatingRoundedToNearestHalf: function() {
+    return (Math.round(this.get('averageRating') * 2) / 2).toFixed(1);
+  }.property('averageRating')
 });
