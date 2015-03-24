ab5a4aa81b835bf7d31d7d1ec8b4f1604c147514
Geoff Schorkopf <gschorkopf@gmail.com>
2015-03-24 18:21:03 -0400

    Writes basic HoleStatLog class

---
+++ app/models/hole_stat_log.rb
@@ -0,0 +1,54 @@
+class HoleStatLog
+  attr_reader :hole, :user
+
+  def initialize(hole: hole, user: user)
+    @hole = hole
+    @user = user
+  end
+
+  def ranking
+    return nil if played_turns_count.zero?
+    logs = hole.course.holes.map do |hole|
+      HoleStatLog.new(hole: hole, user: user)
+    end
+
+    logs = logs.sort_by(&:average_shooting)
+    logs.map(&:hole).find_index(hole) + 1
+  end
+
+  def best_shooting
+    played_turns.map(&:shooting).min
+  end
+
+  def worst_shooting
+    played_turns.map(&:shooting).max
+  end
+
+  def average_score
+    return nil if played_turns_count.zero?
+    played_turns.sum(:score) / played_turns_count.to_f
+  end
+
+  def average_par
+    return nil if played_turns_count.zero?
+    played_turns.sum(:par) / played_turns_count.to_f
+  end
+
+  def average_shooting
+    return nil if played_turns_count.zero?
+    (average_score - average_par)
+  end
+
+  private
+
+  def played_turns_count
+    played_turns.count
+  end
+
+  def played_turns
+    hole.turns
+      .joins(:scorecard)
+      .where(scorecards: { user_id: user.id })
+      .played
+  end
+end
