aa65bfaffe7e46342deb1d6e3b2ae8ea190e9e58
Geoff Schorkopf <gschorkopf@gmail.com>
2015-03-31 14:44:13 -0400

    Adds a hole_stat_logs#index action

--- app/controllers/api/hole_stat_logs_controller.rb
+++ app/controllers/api/hole_stat_logs_controller.rb
@@ -1,6 +1,12 @@
 class Api::HoleStatLogsController < ApplicationController
   respond_to :json
 
+  def index
+    course = Course.find(params[:course_id])
+
+    respond_with :api, course.holes, each_serializer: HoleStatLogSerializer
+  end
+
   def show
     hole = Hole.find(params[:id])
 
