b6d13f2d06f2833688012c399a2d1cac7c455bb4
Geoff Schorkopf <gschorkopf@gmail.com>
2015-04-05 12:23:02 -0500

    Turns out this is the proper way to serialize metadata

--- app/controllers/api/friends_controller.rb
+++ app/controllers/api/friends_controller.rb
@@ -5,10 +5,7 @@ class Api::FriendsController < ApplicationController
     if params[:page].present?
       friends = current_user.friends.order(:first_name, :last_name).page(params[:page])
 
-      render json: {
-        meta: { total_pages: friends.total_pages },
-        friends: friends
-      }
+      respond_with friends, meta: { total_pages: friends.total_pages }
     else
       respond_with current_user.friends
     end
