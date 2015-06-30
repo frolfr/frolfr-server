37b2e2bf0e3ec16d2a4cd5e503ba6db6f1d84d58
Geoffrey Schorkopf <gschorkopf@gmail.com>
2015-06-29 20:17:46 -0400

    Adds rating to CourseScorecardSerializer

--- app/serializers/course_scorecard_serializer.rb
+++ app/serializers/course_scorecard_serializer.rb
@@ -1,5 +1,10 @@
 class CourseScorecardSerializer < ActiveModel::Serializer
-  attributes :id, :total_strokes, :total_score, :created_at, :round_id, :is_completed
+  attributes :id, :total_strokes, :total_score, :created_at, :round_id, :is_completed,
+    :rating
+
+  def rating
+    object.rating
+  end
 
   def created_at
     object.round.created_at
