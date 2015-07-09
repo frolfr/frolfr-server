e83a6da1170db2623aa588533cab226ad79e6a5b
Geoffrey Schorkopf <gschorkopf@gmail.com>
2015-07-09 17:55:28 -0400

    Adds nearest_courses#index resource
    [#177]

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Frolfr::Application.routes.draw do
       json.resources :courses
       json.get 'users/current', to: 'current_users#show'
       json.get 'rounds/current', to: 'current_rounds#show'
+      json.resources :nearest_courses
       json.resources :friends
       json.resources :user_stat_logs
       json.resources :course_stat_logs
