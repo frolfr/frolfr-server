75872f500739fee9b9c5deda83c181a2d7fa014d
Geoffrey Schorkopf <gschorkopf@gmail.com>
2015-07-14 18:14:27 -0400

    Writes scorecards#create resource

--- app/controllers/api/scorecards_controller.rb
+++ app/controllers/api/scorecards_controller.rb
@@ -6,8 +6,29 @@ class Api::ScorecardsController < ApplicationController
     respond_with scorecards
   end
 
+  def create
+    @scorecard = Scorecard.new(scorecard_params)
+
+    if @scorecard.save
+      create_turns!
+      respond_with :api, @scorecard
+    end
+  end
+
   def show
     scorecard = Scorecard.find(params[:id])
     respond_with scorecard
+  end
+
+  private
+
+  def create_turns!
+    @scorecard.course.holes.each do |hole|
+      Turn.create(hole: hole, scorecard: @scorecard, par: hole.par)
+    end
+  end
+
+  def scorecard_params
+    params.require(:scorecard).permit(:round_id, :user_id)
   end
 end
