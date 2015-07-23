0402367a1cc58d2aa6b7e966c6c9506bd82eb368
Geoffrey Schorkopf <gschorkopf@gmail.com>
2015-07-23 18:07:40 -0400

    Stubs out Geocoder for testing env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -76,3 +76,20 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 end
+
+# Geocoder should stub out Google API
+Geocoder.configure(:lookup => :test)
+
+Geocoder::Lookup::Test.set_default_stub(
+  [
+    {
+      'latitude'     => 40.7143528,
+      'longitude'    => -74.0059731,
+      'address'      => 'New York, NY, USA',
+      'state'        => 'New York',
+      'state_code'   => 'NY',
+      'country'      => 'United States',
+      'country_code' => 'US'
+    }
+  ]
+)
