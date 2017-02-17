f2f3162530b2329bce451841b084a19ad9f80149
Geoffrey Schorkopf <gschorkopf@gmail.com>
2017-02-16 21:02:19 -0500

    Make public_recap boolean default to true
    * This kinda sucks, because this part of the client has been removed.
    Oh well.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707002634) do
+ActiveRecord::Schema.define(version: 20170217013949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,7 +72,7 @@ ActiveRecord::Schema.define(version: 20150707002634) do
     t.integer  "course_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "public_recap", default: false, null: false
+    t.boolean  "public_recap", default: true, null: false
   end
 
   add_index "rounds", ["course_id"], name: "index_rounds_on_course_id", using: :btree
