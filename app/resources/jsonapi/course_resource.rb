fff819e02f69a148fb7843cc1dcd6aebdbcac2f8
Geoffrey Schorkopf <gschorkopf@gmail.com>
2017-06-14 22:01:23 -0400

    Adds location filter to course resource

--- app/resources/jsonapi/course_resource.rb
+++ app/resources/jsonapi/course_resource.rb
@@ -10,4 +10,15 @@ class Jsonapi::CourseResource < JSONAPI::Resource
 
     Course.where(id: filtered_record_ids).by_name
   }
+
+  filter :location, apply: ->(records, value, _options) {
+    current_location = CurrentLocation.new(
+      distance_in_miles: 100,
+      latitude: value[0],
+      longitude: value[1]
+    )
+    course_ids = current_location.nearby_courses.first(3).map(&:id)
+
+    Course.where(id: course_ids).by_name
+  }
 end
