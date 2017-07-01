2f9ff3eabece8d83e1f486c404daa5bc4ab64538
Geoffrey Schorkopf <gschorkopf@gmail.com>
2017-07-01 14:41:36 -0600

    Turns should have one scorecard

--- app/resources/jsonapi/turn_resource.rb
+++ app/resources/jsonapi/turn_resource.rb
@@ -2,6 +2,7 @@ class Jsonapi::TurnResource < JSONAPI::Resource
   # TODO Ideally hole_number becomes a column on turns table. As is
   # this will cause n+1 queries
   attributes :hole_number, :par, :strokes
+  has_one :scorecard
 
   # TODO self.updateable_fields will throw a 400. We'd rather just ignore this
   def hole_number=(_); end
