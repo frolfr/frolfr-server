fc97ac84ebeefa0e2cf5fe97ed3a6f6efba1111d
Geoffrey Schorkopf <gschorkopf@gmail.com>
2019-02-04 18:26:04 -0500

    Adds "id_not", a feature that should probably be in jsonapi-resources

--- app/resources/jsonapi/user_resource.rb
+++ app/resources/jsonapi/user_resource.rb
@@ -3,13 +3,17 @@ class Jsonapi::UserResource < JSONAPI::Resource
      :middle_name, :password
   has_many :scorecards
 
+  filter :id_not, apply: ->(records, value, _options) {
+    # TODO: Is there a way to filter without this?
+    records.where.not(id: value)
+  }
   filter :name, apply: ->(records, value, _options) {
     # TODO: this can be cleaned up
     filtered_record_ids = records.select do |user|
       user.full_name.downcase.include? value[0].downcase
     end.map(&:id)
 
-    User.where(id: filtered_record_ids)
+    records.where(id: filtered_record_ids)
   }
 
   def self.apply_sort(records, order_options, context = {})
