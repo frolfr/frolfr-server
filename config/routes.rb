8a580cd4f02ae5fe5e8d17f8b31c710c815c14cc
Chris McCuller <chris@themccullers.com>
2023-04-28 19:01:30 -0400

    :recycle: ensure frolfr-web can authenticate properly

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
   namespace :jsonapi do
-    jsonapi_resources :courses, only: [:show, :index, :create]
-    jsonapi_resources :rounds, only: [:show, :index, :create]
-    jsonapi_resources :scorecards, only: [:show, :index]
-    jsonapi_resources :turns, only: [:show, :index, :update]
-    jsonapi_resources :users, only: [:show, :index, :create]
+    jsonapi_resources :courses, only: %i[show index create]
+    jsonapi_resources :rounds, only: %i[show index create]
+    jsonapi_resources :scorecards, only: %i[show index]
+    jsonapi_resources :turns, only: %i[show index update]
+    jsonapi_resources :users, only: %i[show index create]
   end
 
   post 'login', to: 'jsonapi/sessions#create'
+  post '/api/authorizations', to: 'jsonapi/sessions#create'
 
-  match '/', via: [:options],
-    to:  "application#cors_preflight_check"
-  match '*unmatched', via: [:options],
-    to:  "application#cors_preflight_check"
-
+  match '/', via: [:options], to: 'application#cors_preflight_check'
+  match '*unmatched', via: [:options], to: 'application#cors_preflight_check'
 end
